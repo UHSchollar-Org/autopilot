81914718877c0007ad60de86cab7b53719fc4a85
Karlos Alejandro Alfonso <karlos.bruno99@gmail.com>
2022-11-04 23:53:14 -0400

    some elements added to map

---
+++ source/environment/traffic_signs.py
@@ -0,0 +1,37 @@
+
+class signal:
+    def __str__(self) -> str:
+        return "generic_signal"
+
+class traffic_light(signal):
+    def __str__(self) -> str:
+        return "traffic_light"
+
+class priority(signal):
+    def __str__(self) -> str:
+        return "priority"
+
+class stop(signal):
+    def __str__(self) -> str:
+        return "stop"
+
+class yield_to(signal):
+    def __str__(self) -> str:
+        return "yield_to"
+
+class speed_obligations(signal):
+    def __str__(self) -> str:
+        return "generic_speed_obligation"
+
+class max_speed(speed_obligations):
+    def __str__(self) -> str:
+        return "max_speed"
+
+class min_speed(speed_obligations):
+    def __str__(self) -> str:
+        return "min_speed"
+
+class kepp_speed(speed_obligations):
+    def __str__(self) -> str:
+        return "keep_speed"
+
