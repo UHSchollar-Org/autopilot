beb026475681e276ebbe1d0ec0e20a674c50c1bd
Karlos Alejandro Alfonso <karlos.bruno99@gmail.com>
2022-12-30 00:45:58 -0500

    changes in printers

--- source/environment/console_printer.py
+++ source/environment/console_printer.py
@@ -1,26 +1,13 @@
-
-def print_streets(map):
-    names = []
-    for u,v,name in map.simple_map.edges(data= "name"):
-        if not names.__contains__(name):
-            names.append(name)
-    names.sort()
-    print(names)
+from source.environment._map import *
+def print_streets(map: map):
+    for street in map.streets:
+        print(street)
                 
-def print_signs(map):
-    for n, nbrsdict in map.simple_map.adjacency():
-        for nbr, eattr in nbrsdict.items():
-            to_print = ""
-            to_print += str(n) + " -> " + str(nbr) + " : "
-                
-            for s in eattr.get("signs"):
-                to_print += (str(s) + ",")
-            print(to_print)
+def print_signs(map: map):
+    for street in map.streets:
+        #print(f'street: {street} <signs : {street.traffic_signs}>')
+        pass
 
 def print_street_len(map):
-    for n, nbrsdict in map.simple_map.adjacency():
-        for nbr, eattr in nbrsdict.items():
-            to_print = ""
-            to_print += str(n) + " -> " + str(nbr) + " : " + str(eattr.get("length"))
-
-            print(to_print)
+    for street in map.streets:
+        print(f'street: {street} <length : {street.length}>')
