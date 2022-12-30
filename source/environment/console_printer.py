eabce48e17b21c7df61b1514af5db8793fca5df0
Karlos Alejandro Alfonso <karlos.bruno99@gmail.com>
2022-12-30 01:00:17 -0500

    print signs

--- source/environment/console_printer.py
+++ source/environment/console_printer.py
@@ -1,13 +1,14 @@
 from source.environment._map import *
+
 def print_streets(map: map):
     for street in map.streets:
         print(street)
                 
 def print_signs(map: map):
     for street in map.streets:
-        #print(f'street: {street} <signs : {street.traffic_signs}>')
-        pass
+        print(f'street: {street} <signs : {street.traffic_signs}>')
+        
 
-def print_street_len(map):
+def print_street_len(map : map):
     for street in map.streets:
         print(f'street: {street} <length : {street.length}>')
