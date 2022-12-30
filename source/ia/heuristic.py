5c7486bee9bdc99b4fe3980516e9b63f507af5bc
Karlos Alejandro Alfonso <karlos.bruno99@gmail.com>
2022-12-30 00:44:34 -0500

    euclidean heuristic implemented

---
+++ source/ia/heuristic.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+from source.tools.general_tools import distance_from_geo_coord
+
+class heuristic(ABC):
+    @abstractmethod
+    def evaluate(self, *params):
+        raise NotImplementedError
+
+class euclidean(heuristic):
+    def evaluate(self, start, end):
+        return distance_from_geo_coord(start.geo_coord[0], start.geo_coord[1], end.geo_coord[0], end.geo_coord[1])
