f22c40dff68c831a4496946bbcd8711f00de7e29
Karel Camilo Manresa <karelcamilo@icloud.com>
2023-01-08 20:15:43 -0500

    Pilang_sim now receive cars as parmas

--- source/pilot_dsl/builtins/pilang_sim.py
+++ source/pilot_dsl/builtins/pilang_sim.py
@@ -1,4 +1,5 @@
 from source.simulation.simulation import simulation
+from source.agents.car import car
 from source.tools.reader import map_from_json
 from source.pilot_dsl.builtins.pilang_callable import pilang_callable
 from source.pilot_dsl.builtins.pilang_garage import pilang_gar
@@ -11,10 +12,14 @@ class pilang_sim(pilang_callable):
         return 3
     
     def call(self, interpreter, arguments: List[Any]):
-        if not isinstance(arguments[0], int):
-            raise runtime_error(arguments[0], f'{arguments[0]} most be an integer')
-        if not isinstance(arguments[1], pilang_gar):
+        if not isinstance(arguments[0], pilang_gar):
             raise runtime_error(arguments[1], f'{arguments[1]} most be garages')
-        if not isinstance(arguments[2], int):
+        if not isinstance(arguments[1], int):
             raise runtime_error(arguments[2], f'{arguments[2]} most be an integer')
-        return simulation(map_from_json("Map"),arguments[0],len(arguments[0]),arguments[1].garages, arguments[2])
+        
+        cars = []
+        for i in range(2,len(arguments)):
+            if not isinstance(arguments[i], car):
+                raise runtime_error(arguments[i], f'{arguments[i]} most be a car')
+            cars.append(arguments[i])
+        return simulation(map_from_json("Map"),cars, len(cars), arguments[0].garages, arguments[1])
